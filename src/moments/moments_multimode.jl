cab178a1911030186731b5fe4c8531a3b6d1c7b4
pol-floriach <polfloridt@gmail.com>
2024-06-04 22:28:30 +0200

    Intentat arreglar bug al steady state del multimode

--- src/moments/moments_multimode.jl
+++ src/moments/moments_multimode.jl
@@ -1,14 +1,20 @@
-# Script for time evolution of the moments and variances (SDE system) including 3 modes.TODO: test steady state
+# Script for time evolution of the moments and covariances (SDE system) including 3 modes.TODO: Add appropriate initial conditions
 using DifferentialEquations, Plots, LaTeXStrings
 
+# Function to obtain O_k index of matrix (in a row or column)
+function right_index(O,k,target= 1)
+    return O == target ?  2k-1 : 2k
+end
 # Function to get index of a matrix stored in an array in column-major order (for 6x6 matrix)
 colmaj(row, col) = row + (col-1)*6
+
 # Kronecker delta function for better legibility
 δ(x,y) = ==(x,y)
-# Time evolution w/o information gain TODO: canviar parametres als que toca
+
+# Time evolution w/o information gain. TODO If too many allocations: try with staticarray / add Mi, Nj etc as parameters and mutate
 function moments_evolution_3modes(du, u, p, t)
     # Parameters
-    @views γ0, ω, k, η = p
+    @views γ0, ωm, ωs, Γba ,Γmeas = p
     # X, Y, C
     Xs = @view u[1:2:5]
     Ys = @view u[2:2:6]
@@ -19,59 +25,56 @@ function moments_evolution_3modes(du, u, p, t)
     dC = @view du[7:end]
 
     #First moments
-    dXs = -γ0/2*Xs + (ω - ωm)*Ys
-    dYs = -γ0/2*Ys - (ω - ωm)*Xs
+    @. dXs = -γ0/2*Xs + (ωs - ωm)*Ys
+    @. dYs = -γ0/2*Ys - (ωs - ωm)*Xs
 
     # Covariance matrix
     for M = 1:2, N = 1:2
         for i in 1:3, j in 1:3
-            Mi = M == 1 ?  2*(i-1)+1 : 2*i
-            Nj = N == 1 ?  2*(j-1)+1 : 2*j
-            Mconji = M == 1 ?  2*i : 2*(i-1)+1
-            Nconjj = N == 1 ?  2*j : 2*(j-1)+1
+            # Getting the right indices
+            Mi = right_index(M,i)
+            Nj = right_index(N,j)
+            Mconji = right_index(M,i,2)
+            Nconjj = right_index(N,j,2)
 
-            dC[colmaj(Mi, Nj)] = - (γ0[i]+γ0[j])/2 * C[colmaj(Mi,Nj)] + δ(Mi,Nj)*γ0*(nth+0.5) + δ(M,N)*sqrt(Γba[i]*Γba[j]) 
-                     + (-1)^δ(M,2)*(ω[i] - ωm)*C[colmaj(Mconji, Nj)] + (-1)^δ(N,2)*(ω[j] - ωm)*C[colmaj(Mi, Nconjj)] 
-                     -4*(sum([sqrt(Γmeas[k])*C[colmaj(Mi, 2*k)] for k in 1:3]))*(sum([sqrt(Γmeas[l])*C[colmaj(Nj, 2*k)] for Xl in 1:3]))
-                     -4*(sum([sqrt(Γmeas[k])*C[colmaj(Mi, 2*(k-1)+1)] for k in 1:3]))*(sum([sqrt(Γmeas[l])*C[colmaj(Nj, 2*(k-1)+1)] for Yl in 1:3]))
+            dC[colmaj(Mi, Nj)] = - (γ0[i]+γ0[j])/2 * C[colmaj(Mi,Nj)] + δ(Mi,Nj)*γ0[i]*(nth+0.5) + δ(M,N)*sqrt(Γba[i]*Γba[j]) +
+                     + (-1)^δ(M,2)*(ωs[i] - ωm)*C[colmaj(Mconji, Nj)] + (-1)^δ(N,2)*(ωs[j] - ωm)*C[colmaj(Mi, Nconjj)] +
+                     -4*(sum([sqrt(Γmeas[k])*C[colmaj(Mi, 2k)] for k in 1:3]))*(sum([sqrt(Γmeas[l])*C[colmaj(Nj, 2l)] for l in 1:3])) +
+                     -4*(sum([sqrt(Γmeas[k])*C[colmaj(Mi, 2k-1+1)] for k in 1:3]))*(sum([sqrt(Γmeas[l])*C[colmaj(Nj, 2l-1)] for l in 1:3]))
         end
     end
     nothing
 end
 
-function moments_infogain(u,p,t)
-    k, η = p[3:4]
-    Vx, Cxp = @view u[3:2:5]
-   sqrt(8*k*η)*SA[Vx, Cxp, 0, 0, 0]
-end
-
 function infogain_3modes(du,u,p,t)
-
-    Xs = @view u[1:2:5]
-    Ys = @view u[2:2:6]
     C = @view u[7:end]
     
-    du[1:2:5,1] = [sum([sqrt(Γmeas[j])*C[colmaj(Xi,2*(j-1)+1)] for j in 1:3]) for Xi in 1:2:5] # dWx in d<Xi>
-    du[1:2:5,2] = [sum([sqrt(Γmeas[j])*C[colmaj(Xi,2*j)]       for j in 1:3]) for Xi in 1:2:5] # dWy in d<Xi>
-    du[2:2:6,1] = [sum([sqrt(Γmeas[j])*C[colmaj(Yi,2*(j-1)+1)] for j in 1:3]) for Yi in 2:2:6] # dWx in d<Yi>
-    du[2:2:6,2] = [sum([sqrt(Γmeas[j])*C[colmaj(Yi,2*j)]       for j in 1:3]) for Yi in 2:2:6] # dWy in d<Yi>
+    du[1:2:5,1] = 2*[sum([sqrt(Γmeas[j])*C[colmaj(Xi,2*(j-1)+1)] for j in 1:3]) for Xi in 1:2:5] # dWx in d<Xi>
+    du[1:2:5,2] = 2*[sum([sqrt(Γmeas[j])*C[colmaj(Xi,2*j)]       for j in 1:3]) for Xi in 1:2:5] # dWy in d<Xi>
+    du[2:2:6,1] = 2*[sum([sqrt(Γmeas[j])*C[colmaj(Yi,2*(j-1)+1)] for j in 1:3]) for Yi in 2:2:6] # dWx in d<Yi>
+    du[2:2:6,2] = 2*[sum([sqrt(Γmeas[j])*C[colmaj(Yi,2*j)]       for j in 1:3]) for Yi in 2:2:6] # dWy in d<Yi>
 end
 
 
 
-# Parameters TODO: Convert to vectors
+# Parameters
 ωm = 2π*1.1
-Q = 1e7
-γ0 = ωm / Q
+ωs = [2π*1.1, 2π*(1.1 - 1e-2), 2π*(1.1 + 1e-2)]
+Qs  = [1e7, 1e7, 1e7]
+γ0 = ωs ./ Qs
 
-# Measurement "rate" TODO: Convert to vector
+
+# Measurement "rates"
 g0 = 2π*465
 ncav = 1e5
 g = g0*sqrt(ncav)
 κ = 2π*45e6
 Γqba = 4*g^2/κ /1e6
-k = Γqba/2
 η = 0.9
+
+Γba_vec = [Γqba, Γqba, Γqba]
+Γmeas_vec = [Γqba*η, Γqba*η, Γqba*η]
+
 
 # Constants
 ħ = 1.05457182e-34 * 1e9^2 / 1e6
@@ -81,9 +84,11 @@ const nth = 1/(exp(ħ*ωm/(kB*T))-1)
 Vx_th = Vp_th = nth + 0.5
 
 # Initial conditions and SDE parameters TODO: Change initial conditions to correct size
-p = (γ0, ω, k, η)
-u0 = SA[0, 0, Vx_th, Vp_th, 0.0]
-tspan = (0.0, 100) # μs
+p = (γ0, ωm, ωs, Γba_vec ,Γmeas_vec)
+
+u0 = randn(42)
+
+tspan = (0.0, 10) # μs
 
 prob= SDEProblem(moments_evolution, moments_infogain, u0, tspan, p)
 @time sol = solve(prob,
